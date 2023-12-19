2ad4d49d65d1300dcc0d4fa42754f38afcd194f1
rajat-gith <rajatnayak1582002@gmail.com>
2023-12-19 17:53:51 +0530

    Implemented Authentication

---
+++ controllers/authController.js
@@ -0,0 +1,78 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const jwtSecret = process.env.JWT_SECRET;
+const bcryptSalt = bcrypt.genSaltSync(10);
+
+async function login(req, res) {
+  try {
+    const { username, password } = req.body;
+    const foundUser = await User.findOne({ username });
+
+    if (foundUser) {
+      const passOk = bcrypt.compareSync(password, foundUser.password);
+      if (passOk) {
+        jwt.sign(
+          { userId: foundUser._id, username },
+          jwtSecret,
+          {},
+          (err, token) => {
+            if (err) throw err;
+            res
+              .cookie("token", token, { sameSite: "none", secure: true })
+              .json({
+                id: foundUser._id,
+              });
+          }
+        );
+      }
+    } else {
+      res.status(401).json("Invalid credentials");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Error during login");
+  }
+}
+
+async function logout(req, res) {
+  res
+    .cookie("token", "", { sameSite: "none", secure: true })
+    .json("Logout successful");
+}
+
+async function register(req, res) {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
+    const createdUser = await User.create({
+      username: username,
+      password: hashedPassword,
+    });
+
+    jwt.sign(
+      { userId: createdUser._id, username },
+      jwtSecret,
+      {},
+      (err, token) => {
+        if (err) throw err;
+        res
+          .cookie("token", token, { sameSite: "none", secure: true })
+          .status(201)
+          .json({
+            id: createdUser._id,
+          });
+      }
+    );
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Error during registration");
+  }
+}
+
+module.exports = {
+  login,
+  logout,
+  register,
+};
